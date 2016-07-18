213af44d84d13519fcba8ea0b4139945e6dce103
Mihai <mihai.banulescu@gmail.com>
2016-07-17 17:51:56 -0700

    added constants for review form

---
+++ config/initializers/constants.rb
@@ -0,0 +1,13 @@
+INTEREST_OPTIONS = [
+  ['Zero', 0],
+  ['Some', 1],
+  ['Considerable', 2],
+  ['Tons', 3],
+  ['---', 4],
+  ['Already Completed', 5]
+]
+
+# enum clarity: [:poor, :okay, :great, :impressive]
+# enum difficulty_anticipated: ['far too easy', 'too easy', 'just right', 'too hard']
+# # enum difficulty_hindsight: ['far too easy', 'too easy', 'just right', 'too hard']
+# enum relevance: ['yes', 'maybe', 'no']
