a45a9f4e7cf6e71fcdf03f51ed8823ac05ab6c3a
Mihai <mihai.banulescu@gmail.com>
2016-07-17 22:09:00 -0700

    include hidden searchable description column (!)

--- app/assets/javascripts/goals.js
+++ app/assets/javascripts/goals.js
@@ -6,9 +6,15 @@ $(document).ready(function(){
         //     { targets: 'nosort', orderable: false}
         // ],
         responsive: true,
-        order: [0, 'asc'],
+        order: [1, 'asc'],
         // pageLength: -1,
         // lengthMenu: [ [10, 20, 30, 50, -1], [10, 20, 30, 50, "All"] ],
-        language: { lengthMenu: "Display _MENU_ Goals" }
+        language: { lengthMenu: "Display _MENU_ Goals" },
+        columnDefs: [
+          {
+            targets: [ 0 ],
+            visible: false
+          }
+        ]
     });
 });
